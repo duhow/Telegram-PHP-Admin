66acf33a8846674f51577ba4a94ab5e323f4becf
duhow <contacto@duhowpi.net>
2017-03-22 09:02:38 +0100

    WIP Setup and code cleanup.

--- setup.php
+++ setup.php
@@ -1,13 +1,27 @@
 <?php
 
 $web = file_get_contents("render.html");
+$config = array();
 
-if(isset($_POST) && !empty($_POST)){
-	if(isset($_POST['test-token']) && !empty($_POST['test-token'])){
+function checkvar($v){ return (isset($v) && !empty($v)); }
+
+if(checkvar($_POST)){
+	if(checkvar($_POST['test-token'])){
 		$url = "https://api.telegram.org/bot" .$_POST['test-token'] ."/getMe";
 		$data = file_get_contents($url);
 		header("Content-Type: application/json");
 		die($data);
+	}elseif(checkvar($_POST['token']) && checkvar($_POST['user']) && checkvar($_POST['passwd'])){
+		$hash = $_POST['user'] .":" .password_hash($_POST['passwd'], PASSWORD_BCRYPT, ['cost' => 5]);
+		$fp = fopen(".htpasswd", 'a');
+		fwrite($fp, $hash ."\n");
+		fclose($fp);
+
+		// TODO Save Config
+		// http://stackoverflow.com/a/2237315
+
+		$files = ['setup.html', 'setup.php'];
+		// foreach($files as $f){ unlink($f); }
 	}
 }
 
